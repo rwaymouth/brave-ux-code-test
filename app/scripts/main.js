d7019ed1092a7d350c36657f578ec4280e034dc5
Rob Waymouth <rob.waymouth@mcmurrytmg.com>
2015-06-30 08:58:54 -0400

    Re-organized sass/js to be more readable. Update readme to describe code setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,85 +1,93 @@
-
-
-function togglePassword(target) {
-   var input =  document.querySelector(target);
-   var type = input.attributes.type.nodeValue;
-
-   if (type === 'password') {
-       type = 'text'; 
-   } else {
-        type = 'password';
-   }
-   input.attributes.type.nodeValue = type;
-}
-
-function toggleMenu(target) {
-    var target = document.querySelector(target);
-    var originalClass = target.className;
-    
-    if (originalClass.indexOf('active') > 0) {
-        var newClass = originalClass.replace('active', '');
-    } else {
-        var newClass = originalClass + ' active';
-    }
-    
-    target.className = newClass;
-
-}
-
-function validateUserName(entry) {
-    if (entry.length === 0) {
-        var originalClass = document.querySelector('#username').parentNode.className;
-        var newClass = originalClass + ' error';
-        document.querySelector('#username').parentNode.className = newClass;
-        return false;
-    } else {
-        var originalClass = document.querySelector('#username').parentNode.className;
-        var newClass = originalClass.replace(' error', '');
-        document.querySelector('#username').parentNode.className = newClass;
-        return true;
-    }    
-}
-
-function validatePassword(entry) {
-    if (entry.length < 6 || entry.match(/[A-Z]/) < 0 || entry.match(/[^a-zA-Z\d\s:]/) < 0) {
-        var originalClass = document.querySelector('#password').parentNode.className;
-        var newClass = originalClass + ' error';
-        document.querySelector('#password').parentNode.className = newClass;
-        return false;
-    } else {
-        var originalClass = document.querySelector('#password').parentNode.className;
-        var newClass = originalClass.replace(' error', '');
-        document.querySelector('#password').parentNode.className = newClass;
-        return true;
-    }    
-}
-
+// Elements
 var pwToggle = document.querySelector('.eye--icon');
 var menuToggle = document.querySelector('.nav--toggle');
 var submit = document.querySelector('.brave-ux--button');
 
-window.onload = function() {
-    pwToggle.addEventListener('click', function(){togglePassword('#password')}, false);
-    menuToggle.addEventListener('click', function(){toggleMenu('.app--nav')}, false);
-    submit.addEventListener('click', function(){
-        var userName = document.querySelector('#username').value;
-        var password = document.querySelector('#password').value;
-        var validName = validateUserName(userName);
-        var validPass = validatePassword(password);
-        if (!validName || !validPass) {
-            var message = "Your username or password wasn't recognized.";
-            var classes = 'error';
-        } else {
-            var message = "Succes!";
-            var classes = 'success';
-        }
-        document.querySelector('.modal--message').innerHTML = message;
-        document.querySelector('.app--modal').className = 'app--modal ' + classes;
-        
-        window.setTimeout(function(){
-            document.querySelector('.app--modal').className = 'app--modal hidden';
-        }, 3000)
-    })
+// Helper Functions
+// Opportunity exists to re-factor some of these for a DRYer codebase.
+function togglePassword(target) {
+  var input = document.querySelector(target);
+  var type = input.attributes.type.nodeValue;
+
+  if (type === 'password') {
+    type = 'text';
+  } else {
+    type = 'password';
+  }
+  input.attributes.type.nodeValue = type;
 }
 
+function toggleMenu(target) {
+  var target = document.querySelector(target);
+  var originalClass = target.className;
 
+  if (originalClass.indexOf('active') > 0) {
+    var newClass = originalClass.replace('active', '');
+  } else {
+    var newClass = originalClass + ' active';
+  }
+
+  target.className = newClass;
+}
+
+function validateUserName(entry) {
+  if (entry.length === 0) {
+    var originalClass = document.querySelector('#username').parentNode.className;
+    var newClass = originalClass + ' error';
+    document.querySelector('#username').parentNode.className = newClass;
+    return false;
+  } else {
+    var originalClass = document.querySelector('#username').parentNode.className;
+    var newClass = originalClass.replace(' error', '');
+    document.querySelector('#username').parentNode.className = newClass;
+    return true;
+  }
+}
+
+function validatePassword(entry) {
+  if (entry.length < 6 || entry.match(/[A-Z]/) < 0 || entry.match(/[^a-zA-Z\d\s:]/) < 0) {
+    var originalClass = document.querySelector('#password').parentNode.className;
+    var newClass = originalClass + ' error';
+    document.querySelector('#password').parentNode.className = newClass;
+    return false;
+  } else {
+    var originalClass = document.querySelector('#password').parentNode.className;
+    var newClass = originalClass.replace(' error', '');
+    document.querySelector('#password').parentNode.className = newClass;
+    return true;
+  }
+}
+
+// Init
+window.onload = function() {
+  pwToggle.addEventListener('click', function() {
+    togglePassword('#password')
+  }, false);
+
+  menuToggle.addEventListener('click', function() {
+    toggleMenu('.app--nav')
+  }, false);
+
+  submit.addEventListener('click', function() {
+    var userName = document.querySelector('#username').value;
+    var password = document.querySelector('#password').value;
+    var validName = validateUserName(userName);
+    var validPass = validatePassword(password);
+
+    // If there is a non-valid entry set the appropriate message.
+    if (!validName || !validPass) {
+      var message = "Your username or password wasn't recognized.";
+      var classes = 'error';
+    } else {
+      var message = "Succes!";
+      var classes = 'success';
+    }
+    document.querySelector('.modal--message').innerHTML = message;
+    document.querySelector('.app--modal').className = 'app--modal ' + classes;
+
+    // Show the modal for 3sec.
+    window.setTimeout(function() {
+      document.querySelector('.app--modal').className = 'app--modal hidden';
+    }, 3000)
+  })
+}
--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ This site was built using the Yeoman Gulp-Webapp generator as a starting point. 
 
 ``npm install`` then `gulp brave-ux`.
 
+## Notes
+- Styles are using display-table for fine-grain control over elements due to the requirement to support IE9. If that requirement didn't exist we could use flexbox to simplify the layout.
+- Sass files are grouped in a config folder for variables, modules folder for the individual modules.
+- There's defintiely some room to re-factor the helper functions in the main.js file. Would recommend taking another pass over it as time allows. It works ATM, so this is not critical.
